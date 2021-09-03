8015c13216345b38128df8bc7ceaa18460e1eb3c
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-09-03 11:57:41 +0100

    added react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
-  return <div>Hi</div>
+  return (
+    <Switch>
+      <Route path="/" exact>
+        Home Page
+      </Route>
+      <Route path="/starred" exact>
+        Starred Page
+      </Route>
+      <Route>This is 404 page.</Route>
+    </Switch>
+  );
 }
 
-export default App
+export default App;
