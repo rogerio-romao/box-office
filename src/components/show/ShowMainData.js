1c51c8aa577fed6e725b72b1ae5428f061f6a48b
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-09-07 00:23:22 +0100

    styled show page

--- src/components/show/ShowMainData.js
+++ src/components/show/ShowMainData.js
@@ -1,33 +1,37 @@
 import React from 'react';
 
 import { Star } from '../styled';
+import { MainDataWrapper, TagList } from './ShowMainData.styled';
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png';
 
 const ShowMainData = ({ name, rating, summary, tags, image }) => {
   return (
-    <div>
+    <MainDataWrapper>
       <img src={image ? image.original : IMAGE_NOT_FOUND} alt="show-cover" />
-      <div>
-        <div>
+      <div className="text-side">
+        <div className="headline">
           <h1>{name}</h1>
           <div>
             <Star />
             <span>{rating.average || 'N/A'}</span>
           </div>
         </div>
-        <div dangerouslySetInnerHTML={{ __html: summary }} />
+        <div
+          className="summary"
+          dangerouslySetInnerHTML={{ __html: summary }}
+        />
 
         <div>
           Tags:{' '}
-          <div>
+          <TagList>
             {tags.map((tag, i) => (
               <span key={i}>{tag}</span>
             ))}
-          </div>
+          </TagList>
         </div>
       </div>
-    </div>
+    </MainDataWrapper>
   );
 };
 
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,6 +6,7 @@ import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 import { apiGet } from '../misc/config';
 
@@ -57,7 +58,7 @@ const Show = () => {
   if (error) return <div>An error occured: {error}</div>;
 
   return (
-    <div>
+    <ShowPageWrapper>
       <ShowMainData
         image={show.image}
         name={show.name}
@@ -65,23 +66,23 @@ const Show = () => {
         summary={show.summary}
         tags={show.genres}
       />
-      <div>
+      <InfoBlock>
         <h2>Details</h2>
         <Details
           status={show.status}
           network={show.network}
           premiered={show.premiered}
         />
-      </div>
-      <div>
+      </InfoBlock>
+      <InfoBlock>
         <h2>Seasons</h2>
         <Seasons seasons={show._embedded.seasons} />
-      </div>
-      <div>
+      </InfoBlock>
+      <InfoBlock>
         <h2>Cast</h2>
         <Cast cast={show._embedded.cast} />
-      </div>
-    </div>
+      </InfoBlock>
+    </ShowPageWrapper>
   );
 };
 
