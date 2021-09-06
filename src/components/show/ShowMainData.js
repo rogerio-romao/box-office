a0f86748634a1514560814be9890bedd9397d655
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2021-09-06 14:28:10 +0100

    created show page components and populated them with data

---
+++ src/components/show/ShowMainData.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { Star } from '../styled';
+
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+
+const ShowMainData = ({ name, rating, summary, tags, image }) => {
+  return (
+    <div>
+      <img src={image ? image.original : IMAGE_NOT_FOUND} alt="show-cover" />
+      <div>
+        <div>
+          <h1>{name}</h1>
+          <div>
+            <Star />
+            <span>{rating.average || 'N/A'}</span>
+          </div>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: summary }} />
+
+        <div>
+          Tags:{' '}
+          <div>
+            {tags.map((tag, i) => (
+              <span key={i}>{tag}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ShowMainData;
